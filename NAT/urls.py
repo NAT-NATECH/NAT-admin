208f8bc6a3efef5656766d6c0818caf29e1504f1
Neo <neojdpm@gmail.com>
2017-05-26 16:43:28 -0430

    First Commit

---
+++ NAT/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    ## Backoffice module's URLs
+    url(r'^', include('backoffice.urls'), name='BackofficeApp'),
+    ## Login/Loguot
+    url(r'^accounts/login/$', auth_views.login, {'template_name': 'backoffice/login.html'}),
+    url(r'^accounts/logout/$', auth_views.logout, {'next_page': 'login'})
+]
